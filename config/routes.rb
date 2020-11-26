5c254de6a915af095dabe54f9aced73c4bf5336a
Ubuntu <ubuntu@ip-172-31-6-84.ec2.internal>
2020-11-26 08:23:17 +0000

    Add Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+  
   devise_for :users
+    devise_scope :user do
+      get '/users/sign_out' => 'devise/sessions#destroy'
+    end
+    
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
